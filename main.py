2c6276925a844bb0e086adf28403a8740df08c65
yousan <ka1357amnbpdr@gmail.com>
2024-01-25 01:36:01 +0900

    fix: 起動時にGPUとCPUの自動判断およびCPUの場合は最大スレッドの指定

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from faster_whisper import WhisperModel
+import torch
 import os
 
 app = FastAPI()
-AUDIO_FILE_PATH = '/path/to/your/audiofile.wav'  # 適切なパスに置き換えてください
+
+def initialize_model():
+    if torch.cuda.is_available():
+        # CUDAが利用可能な場合、CUDAデバイスを使用
+        return WhisperModel("large-v3", device="cuda", compute_type="float16")
+    else:
+        # CUDAが利用不可の場合、CPUを使用し、利用可能な最大スレッド数を設定
+        cpu_threads = os.cpu_count()
+        return WhisperModel("large-v3", device="cpu", compute_type="int8", cpu_threads=cpu_threads)
 
 # WhisperModelの初期化（サーバー起動時に一度だけ行う）
-model = WhisperModel("large-v3", device="cuda", compute_type="float16")
+model = initialize_model()
 
 @app.get("/transcribe")
-async def transcribe_audio():
+async def transcribe_audio(file_path: str = Query(..., description="The path to the audio file")):
     # 音声ファイルの文字起こし
-    segments, info = model.transcribe(
-        AUDIO_FILE_PATH,
-        beam_size=5,
-        vad_filter=True,
-        without_timestamps=True,
-    )
+    try:
+        segments, info = model.transcribe(
+            file_path,
+            beam_size=5,
+            vad_filter=True,
+            without_timestamps=True,
+        )
+    except Exception as e:
+        return {"error": str(e)}
 
     # 結果のフォーマット
     result = "Detected language '%s' with probability %f\n" % (info.language, info.language_probability)
@@ -24,6 +36,10 @@ async def transcribe_audio():
         result += "[%.2fs -> %.2fs] %s\n" % (segment.start, segment.end, segment.text)
 
     # ファイルの削除
-    os.remove(AUDIO_FILE_PATH)
+    # try:
+    #     os.remove(file_path)
+    # except OSError as e:
+    #     # ファイル削除時のエラーハンドリング
+    #     return {"error": f"Failed to delete file: {str(e)}"}
 
-    return result
+    return {"transcription": result}
