5be65174d1553f27455a6dae5b27a0484fa8c9f1
yousan <ka1357amnbpdr@gmail.com>
2024-01-25 02:14:13 +0900

    feat: バイナリーデータを渡して処理できるように変更

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, File
 from faster_whisper import WhisperModel
 import torch
 import os
@@ -7,34 +7,31 @@ app = FastAPI()
 
 def initialize_model():
     if torch.cuda.is_available():
-        # CUDAが利用可能な場合、CUDAデバイスを使用
         print("CUDA is available")
         return WhisperModel("large-v3", device="cuda", compute_type="float16")
     else:
-        # CUDAが利用不可の場合、CPUを使用し、利用可能な最大スレッド数を設定
         print("CUDA is not available or not enabled")
         cpu_threads = os.cpu_count()
         return WhisperModel("large-v3", device="cpu", compute_type="int8", cpu_threads=cpu_threads)
 
-# WhisperModelの初期化（サーバー起動時に一度だけ行う）
 model = initialize_model()
 
-@app.get("/transcribe")
-async def transcribe_audio(file_path: str = Query(..., description="The path to the audio file")):
-    # 音声ファイルの文字起こし
+@app.post("/transcribe")
+async def transcribe_audio(file: bytes = File(...)):
     try:
+        # 音声ファイルの文字起こし
         segments, info = model.transcribe(
-            file_path,
+            file,  # ここでバイナリデータを渡す
             beam_size=5,
             vad_filter=True,
             without_timestamps=True,
         )
+
+        # 結果のフォーマット
+        result = "Detected language '%s' with probability %f\n" % (info.language, info.language_probability)
+        for segment in segments:
+            result += "[%.2fs -> %.2fs] %s\n" % (segment.start, segment.end, segment.text)
+
+        return {"transcription": result}
     except Exception as e:
         return {"error": str(e)}
-
-    # 結果のフォーマット
-    result = "Detected language '%s' with probability %f\n" % (info.language, info.language_probability)
-    for segment in segments:
-        result += "[%.2fs -> %.2fs] %s\n" % (segment.start, segment.end, segment.text)
-
-    return {"transcription": result}
