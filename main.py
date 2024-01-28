5a55585920240732f645832e07c36e9968c3ceff
yousan <ka1357amnbpdr@gmail.com>
2024-01-29 00:08:17 +0900

    fix: レスポンスの内容の変更と言語の指定

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, UploadFile, File, Form
-from faster_whisper import WhisperModel
-import torch
-import os
 import io
+import json
+import os
+
+import torch
+from fastapi import FastAPI, UploadFile, Form
+from faster_whisper import WhisperModel
 
 app = FastAPI()
 
@@ -14,7 +16,9 @@ def initialize_model():
     else:
         print("CUDA is not available or not enabled")
         cpu_threads = os.cpu_count()
-        return WhisperModel("large-v3", device="cpu", compute_type="int8", cpu_threads=cpu_threads, download_root=model_path)
+        return WhisperModel("large-v3", device="cpu", compute_type="int8", cpu_threads=cpu_threads,
+                            download_root=model_path)
+
 
 model = initialize_model()
 
@@ -31,15 +35,37 @@ async def transcribe_audio(file: UploadFile = Form(...)):
         segments, info = model.transcribe(
             audio=file_stream,  # BinaryIOオブジェクトを渡す
             beam_size=5,
+            language="ja",
             vad_filter=True,
             without_timestamps=True,
         )
 
-        # 結果のフォーマット
-        result = "Detected language '%s' with probability %f\n" % (info.language, info.language_probability)
-        for segment in segments:
-            result += "[%.2fs -> %.2fs] %s\n" % (segment.start, segment.end, segment.text)
+        # JSONオブジェクトを構築
+        response_json = {
+            "language": info.language,
+            "language_probability": info.language_probability,
+            "translations": []
+        }
 
-        return {"transcription": result}
+        total_time = 0.0
+        for segment in segments:
+            segment_duration = segment.end - segment.start
+            total_time += segment_duration
+
+            response_json["translations"].append({
+                "start_time": segment.start,
+                "end_time": segment.end,
+                "duration": segment_duration,
+                "text": segment.text
+            })
+
+        response_json["total_translation_time"] = total_time
+
+        # JSON形式の文字列に変換
+        json_response = json.dumps(response_json, indent=2, ensure_ascii=False)
+
+        # JSON形式の文字列を出力（または返す）
+        print(json_response)
+        return json_response
     except Exception as e:
         return {"error": str(e)}
